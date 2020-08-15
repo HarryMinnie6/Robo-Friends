3539597f340c9acf9d6a2bb378a7b7e3d46e95b8
HarryMinnie6 <56819025+HarryMinnie6@users.noreply.github.com>
2020-08-15 17:29:12 +0200

    Updates

---
+++ precache-manifest.b10264bca57b87c4759f2eb14615c3a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1629795da8f94c6c249ccb3a8c4c54db",
+    "url": "/Robo-Friends/index.html"
+  },
+  {
+    "revision": "8a86321709fac8242286",
+    "url": "/Robo-Friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "501ff3894b41166f571c",
+    "url": "/Robo-Friends/static/css/main.143c23c5.chunk.css"
+  },
+  {
+    "revision": "8a86321709fac8242286",
+    "url": "/Robo-Friends/static/js/2.e92ee690.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robo-Friends/static/js/2.e92ee690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "501ff3894b41166f571c",
+    "url": "/Robo-Friends/static/js/main.5ef1aa36.chunk.js"
+  },
+  {
+    "revision": "44c80db23c2dfc087671",
+    "url": "/Robo-Friends/static/js/runtime-main.08878097.js"
+  }
+]);
